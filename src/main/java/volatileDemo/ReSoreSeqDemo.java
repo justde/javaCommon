f200d753877a60c0fed8c2fafdd9e93751e055a3
zhangjd <iszhangjd@outlook.com>
2019-11-04 15:23:55 +0800

    test

--- src/main/java/volatileDemo/ReSoreSeqDemo.java
+++ src/main/java/volatileDemo/ReSoreSeqDemo.java
@@ -26,6 +26,7 @@ public class ReSoreSeqDemo {
 
 class test {
     public static void main(String[] args) {
+
         ReSoreSeqDemo r = new ReSoreSeqDemo();
         new Thread(()->{
             r.method2();
